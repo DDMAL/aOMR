d281c2fd62ad992fde8fb6d289d8787470ce727e
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-04-17 01:22:51 -0400

    offset

--- bin/highlight_from_mei.py
+++ bin/highlight_from_mei.py
@@ -37,7 +37,8 @@ if __name__ == "__main__":
         rgb.draw_filled_rect((int(facs.ulx) - 5, int(facs.uly) - 5), (int(facs.lrx) + 5, int(facs.lry) + 5), neumecolour)
         
         note_string = '-'.join([note.pitchname for note in neume.children_by_name('note')])
-        rgb.draw_text((int(facs.ulx) - 5, int(facs.uly) - 5), note_string, RGBPixel(0,0,0), halign="left")
+        print note_string
+        rgb.draw_text((int(facs.ulx) - 20, int(facs.uly) - 20), note_string, RGBPixel(0,0,0), halign="left")
     
     for clef in clefs:
         facs = mdoc.get_by_facs(clef.facs)[0]
