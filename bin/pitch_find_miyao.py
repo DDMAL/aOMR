0ab8eca004dbe0cb81a6e32ef0993db85fa75d43
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-04-25 18:00:11 -0400

    Adding command line option parsing

--- bin/pitch_find_miyao.py
+++ bin/pitch_find_miyao.py
@@ -33,63 +33,80 @@ h.setFormatter(f)
 lg.setLevel(logging.DEBUG)
 lg.addHandler(h)
 
-
-aomr_opts = {
-    'lines_per_staff': 4,
-    'staff_finder': 0, # 0: Miyao
-    'staff_removal': 0,
-    'binarization': 0,
-    'discard_size': 12
-}
-
-#FILES TO PROCESS
-# original_file = "/Users/gabriel/Dropbox/OMR_LU/imgs/OK/1001/original_image.tiff"
-# glyphs = gamera_xml.glyphs_from_xml(r"/Users/gabriel/Dropbox/OMR_LU/imgs/OK/1001/page_glyphs.xml")
-original_file = "/Users/ahankins/Documents/projects/1000/original_image.tiff"
-glyphs = gamera_xml.glyphs_from_xml(r"/Users/ahankins/Documents/projects/1000/page_glyphs.xml")
-file_name = (original_file.split('/')[-2] + '_' + original_file.split('/')[-1])
+def main(original_file, page_file, outdir):
+    aomr_opts = {
+        'lines_per_staff': 4,
+        'staff_finder': 0, # 0: Miyao
+        'staff_removal': 0,
+        'binarization': 0,
+        'discard_size': 12
+    }
+    
+    #FILES TO PROCESS
+    glyphs = gamera_xml.glyphs_from_xml(page_file)
+    file_name = (original_file.split('/')[-2] + '_' + original_file.split('/')[-1])
 
 
-# CREATING AOMR OBJECT, FINDING STAVES, AND RETRIEVING STAFF COORDINATES
-aomr_obj = AomrObject(original_file, **aomr_opts)
-st_position = aomr_obj.find_staves() # staves position
-staff_coords = aomr_obj.staff_coords()
+    # CREATING AOMR OBJECT, FINDING STAVES, AND RETRIEVING STAFF COORDINATES
+    aomr_obj = AomrObject(original_file, **aomr_opts)
+    st_position = aomr_obj.find_staves() # staves position
+    staff_coords = aomr_obj.staff_coords()
 
-sorted_glyphs = aomr_obj.miyao_pitch_find(glyphs, aomr_opts.get('discard_size'))
+    sorted_glyphs = aomr_obj.miyao_pitch_find(glyphs, aomr_opts.get('discard_size'))
 
-# PITCH FINDING
-# pitch_find = aomr_obj.pitch_find(glyphs, st_position, aomr_opts.get('discard_size'))
-# print len(pitch_find)
-# sorted_glyphs = sorted(proc_glyphs, key=itemgetter(1, 2))
+    # PITCH FINDING
+    # pitch_find = aomr_obj.pitch_find(glyphs, st_position, aomr_opts.get('discard_size'))
+    # print len(pitch_find)
+    # sorted_glyphs = sorted(proc_glyphs, key=itemgetter(1, 2))
 
 
 
-# STRUCTURING THE DATA IN JSON
-data = {}
-for s, stave in enumerate(staff_coords):
-    contents = []
-    for sg in sorted_glyphs:
-        # lg.debug("sg[1]:{0} s:{1} sg{2}".format(sg[1], s+1, sg))
-        # structure: g, stave, g.offset_x, note, strt_pos
-        if sg[1] == s+1: 
-            glyph = {   'type': sg[0].get_main_id().split('.')[0],
-                        'form': sg[0].get_main_id().split('.')[1:],
-                        'coord': [sg[0].offset_x, sg[0].offset_y, \
-                                sg[0].offset_x+sg[0].ncols, sg[0].offset_y+sg[0].nrows],
-                        'strt_pitch': sg[4],
-                        'strt_pos': sg[3]}
-            contents.append(glyph)  
-    data[s] = {'coord':stave, 'content':contents}    
-print data
-print
+    # STRUCTURING THE DATA IN JSON
+    data = {}
+    for s, stave in enumerate(staff_coords):
+        contents = []
+        for sg in sorted_glyphs:
+            # lg.debug("sg[1]:{0} s:{1} sg{2}".format(sg[1], s+1, sg))
+            # structure: g, stave, g.offset_x, note, strt_pos
+            if sg[1] == s+1: 
+                glyph = {   'type': sg[0].get_main_id().split('.')[0],
+                            'form': sg[0].get_main_id().split('.')[1:],
+                            'coord': [sg[0].offset_x, sg[0].offset_y, \
+                                    sg[0].offset_x+sg[0].ncols, sg[0].offset_y+sg[0].nrows],
+                            'strt_pitch': sg[4],
+                            'strt_pos': sg[3]}
+                contents.append(glyph)  
+        data[s] = {'coord':stave, 'content':contents}    
+    print data
+    print
 
-# CREATING THE MEI FILE
-mei_file = AomrMeiOutput.AomrMeiOutput(data, file_name)
-# print mei_file
-# print
+    # CREATING THE MEI FILE
+    mei_file = AomrMeiOutput.AomrMeiOutput(data, file_name)
+    # print mei_file
+    # print
+    
+    
+    meitoxml.meitoxml(mei_file.md, os.path.join(outdir, file_name.split('.')[0]+'.mei'))
 
-
-meitoxml.meitoxml(mei_file.md, '/Users/gabriel/Desktop/' + file_name.split('.')[0]+'.mei')
+if __name__ == "__main__":
+    usage = "%prog path_to_image path_to_pagexml output_dir"
+    opts = OptionParser(usage = usage)
+    options, args = opts.parse_args()
+    
+    if not args:
+        opts.error("You must supply arguments to this script.")
+    
+    if not args[0]:
+        opts.error("You must supply a path to an image.")
+    if not args[1]:
+        opts.error("You must supply a pagexml file")
+    if not args[2]:
+        opts.error("You must supply an output directory.")
+    
+    main(args[0], args[1], args[2])
+    
+    
+    
 
 
 
