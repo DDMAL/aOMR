47b0b67a0aa11cc835ac64575fdaeb05020f7ea0
Andrew Hankinson <andrew.hankinson@mail.mcgill.ca>
2011-05-01 11:13:09 -0400

    commenting on GT comparison file

--- bin/comparingGT.py
+++ bin/comparingGT.py
@@ -1,8 +1,5 @@
 from optparse import OptionParser
 from pymei.Import import xmltomei
-from pymei.Export import meitoxml
-from lxml import etree
-
 
 if __name__ == "__main__":
     usage = "usage: %prog [options] input_ground_truth__file input_file_to_compare output_folder"
@@ -13,31 +10,48 @@ if __name__ == "__main__":
     comp_file = args[1]
     output_folder = args[2]
     
-    GT = []
-    COMP = []
-    e = 0
+    # GT = []
+    # COMP = []
+    # e = 0
 
-    xmlGT = xmltomei.xmltomei(gt_file)
-    xmlCMP = xmltomei.xmltomei(comp_file)
-    
-    gt_tree = etree.parse(gt_file)
-    comp_tree = etree.parse(comp_file)
+    # xmlGT = xmltomei.xmltomei(gt_file)
+    # xmlCMP = xmltomei.xmltomei(comp_file)
+    # 
+    # gt_tree = etree.parse(gt_file)
+    # comp_tree = etree.parse(comp_file)
     
     # print (etree.tostring(gt_tree))
     # for element in tree.iter("{http://www.music-encoding.org/ns/mei}neume"):
     #     # print element.attrib['name']
     #     print element.attrib
 
-    for i, element in enumerate(gt_tree.iter('{http://www.music-encoding.org/ns/mei}note')):
-        # print i, element.attrib['pname']
-        GT.append(element.attrib['pname'])
+    # for i, element in enumerate(gt_tree.iter('{http://www.music-encoding.org/ns/mei}note')):
+    #     # print i, element.attrib['pname']
+    #     GT.append(element.attrib['pname'])
+    # 
+    # for i, element in enumerate(comp_tree.iter('{http://www.music-encoding.org/ns/mei}note')):
+    #     # print i, element.attrib['pname']
+    #     COMP.append(element.attrib['pname'])
+    #     if COMP[i] != GT [i]:
+    #         e = e + 1.0
+            
+    # GABRIEL:
+    # Try this:
+    ground_truth = xmltomei.xmltomei(gt_file)
+    comparison = xmltomei.xmltomei(comp_file)
     
-    for i, element in enumerate(comp_tree.iter('{http://www.music-encoding.org/ns/mei}note')):
-        # print i, element.attrib['pname']
-        COMP.append(element.attrib['pname'])
-        if COMP[i] != GT [i]:
-            e = e + 1.0
-        
+    # use the "search" method to find all <note> elements in the document, and 
+    # then create an array of pitches using n.pitchname
+    gt_notes = [n.pitchname for n in ground_truth.search('note')]
+    cp_notes = [c.pitchname for c in comparison.search('note')]
+    
+    print "The number of notes in the ground truth is: {0}".format(len(gt_notes))
+    print "The number of notes in the comparison is {0}".format(len(cp_notes))
+    
+    # You will get a difference between the number of notes in each, meaning you cannot use
+    # indexing, e.g., CMP[i] == GT[i]. For example, if one note is missed in the comparison set, 
+    # every other note comparison will be wrong after that, since you will be off by one.
+    
     # print GT
     # print COMP
     print
